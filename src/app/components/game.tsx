8f382a4c6e19deea273a43af2546654430ac7431
agacia1805 <agata.dopierala@vaimo.com>
2023-11-29 19:54:09 +0000

    (feat) add on click events and comparing logic

--- src/app/components/game.tsx
+++ src/app/components/game.tsx
@@ -1,65 +1,142 @@
 'use client';
 //split code so that only leave children are client components
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
 type playOptions = 'rock' | 'paper' | 'scissors';
 
 export const Game = () => {
-    const [userChoice, setUserChoice] = useState<playOptions>();
+    const [userChoice, setUserChoice] = useState<playOptions | null>(null);
     const [computerChoice, setComputerChoice] = useState<playOptions | null>(null);
     const [gameResult, setGameResult] = useState<'You won' | 'You lost' | 'Draw'>();
+    const [isUserPlaying, setIsUserPlaying] = useState<boolean>(false);
+    const [userScore, setUserScore] = useState<number>(0);
+    const [computerScore, setComputerScore] = useState<number>(0);
+
     const playOptions: playOptions[] = ["rock", "paper", "scissors"];
+
 
     const playComputer = () => {
         const randomOption = playOptions[(Math.floor(Math.random() * playOptions.length))];
         setComputerChoice(randomOption);
     }
 
-    const compareChoices = () => {
-        if (!userChoice) return null;
+//     const compareChoices = () => {
+//             if (!userChoice || !computerChoice) return null;
+//
+//             switch(userChoice) {
+//                 case "rock":
+//                     if(computerChoice === "paper") {
+//                         setGameResult('You lost');
+//                         setComputerScore((prevScore) => prevScore + 1);
+//                     } else if (computerChoice === "scissors") {
+//                         setGameResult('You won');
+//                         setUserScore((prevScore) => prevScore + 1);
+//                     } else {
+//                         setGameResult('Draw');
+//                     }
+//                  break;
+//                 case "paper":
+//                     if(computerChoice === "scissors") {
+//                         setGameResult('You lost');
+//                         setComputerScore((prevScore) => prevScore + 1);
+//                     } else if (computerChoice === "rock") {
+//                         setGameResult('You won');
+//                         setUserScore((prevScore) => prevScore + 1);
+//                     } else {
+//                         setGameResult('Draw');
+//                     }
+//                  break;
+//                 case "scissors":
+//                     if(computerChoice === "rock") {
+//                         setGameResult('You lost');
+//                         setComputerScore((prevScore) => prevScore + 1);
+//                     } else if (computerChoice === "paper") {
+//                         setGameResult('You won');
+//                         setUserScore((prevScore) => prevScore + 1);
+//                     } else {
+//                         setGameResult('Draw');
+//                     }
+//                 break;
+//                 default: break;
+//             }
+//     }
 
-        switch(userChoice) {
+const compareChoices = () => {
+    if (!userChoice || !computerChoice) return null;
 
+    switch(userChoice) {
         case "rock":
             if(computerChoice === "paper") {
                 setGameResult('You lost');
+                setComputerScore((prevScore) => prevScore + 1);
             } else if (computerChoice === "scissors") {
                 setGameResult('You won');
+                setUserScore((prevScore) => prevScore + 1);
             } else {
                 setGameResult('Draw');
             }
+            break; // Ensure this break is here
         case "paper":
             if(computerChoice === "scissors") {
                 setGameResult('You lost');
+                setComputerScore((prevScore) => prevScore + 1);
             } else if (computerChoice === "rock") {
                 setGameResult('You won');
+                setUserScore((prevScore) => prevScore + 1);
             } else {
                 setGameResult('Draw');
             }
+            break; // Ensure this break is here
         case "scissors":
-            if(computerChoice === "paper") {
+            if(computerChoice === "rock") {
                 setGameResult('You lost');
-            } else if (computerChoice === "scissors") {
+                setComputerScore((prevScore) => prevScore + 1);
+            } else if (computerChoice === "paper") {
                 setGameResult('You won');
+                setUserScore((prevScore) => prevScore + 1);
             } else {
                 setGameResult('Draw');
             }
-        break;
-        default: break;
-        }
+            break; // Ensure this break is here
+        default:
+            // Handle the default case appropriately if needed
+            break;
     }
+}
+
+
+     const playUser = (choice: playOptions) => {
+            setIsUserPlaying(true);
+            setUserChoice(choice);
+            const playComputerHandler = setTimeout(() => {
+                playComputer();
+            }, 0);
+
+            return () => {
+                clearTimeout(playComputerHandler);
+            }
+     };
+
+     useEffect(() => {
+        compareChoices();
+        setIsUserPlaying(false);
+//         setComputerChoice(null);
+//         setUserChoice(null);
+     }, [userChoice, computerChoice]);
+
 
 
    return (
    <>
-         <button onClick={playComputer}>Click me button play computer</button>
-         <button onClick={setUserChoice("rock")}>Rock</button>
-         <button onClick={setUserChoice("paper")}>Paper</button>
-         <button onClick={setUserChoice("scissors")}>Scissors</button>
+         <button disabled={isUserPlaying} type="button" name={"rock"} onClick={() => playUser("rock")}>Rock</button>
+         <button disabled={isUserPlaying} type="button" name={"paper"} onClick={(e) => playUser("paper")}>Paper</button>
+         <button disabled={isUserPlaying} type="button" name={"scissors"} onClick={(e) => playUser("scissors")}>Scissors</button>
 
-         <div>{computerChoice}</div>
-         <div>{gameResult}</div>
-
+         <div>User choice: {userChoice}</div>
+         <div>Computer choice: {computerChoice}</div>
+         <div>Game result: {gameResult}</div>
+         <div> Persistent user score: {userScore}</div>
+         <div> Persistent computer score: {computerScore}</div>
          </>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import {Game} from './components/game';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center p-24">
         <Game />
     </main>
   )
