85e6d26b231a6bfcbdb1fbacb97d53c4bf04f259
agacia1805 <agata.dopierala@vaimo.com>
2023-11-30 19:59:56 +0000

    (feat) improve logic and add reset function

--- src/app/components/game.tsx
+++ src/app/components/game.tsx
@@ -1,5 +1,5 @@
 'use client';
-//split code so that only leave children are client components
+//split code so that only leaf children are client components
 import React, {useState, useEffect} from "react";
 
 type playOptions = 'rock' | 'paper' | 'scissors';
@@ -14,52 +14,11 @@ export const Game = () => {
 
     const playOptions: playOptions[] = ["rock", "paper", "scissors"];
 
-
     const playComputer = () => {
         const randomOption = playOptions[(Math.floor(Math.random() * playOptions.length))];
         setComputerChoice(randomOption);
     }
 
-//     const compareChoices = () => {
-//             if (!userChoice || !computerChoice) return null;
-//
-//             switch(userChoice) {
-//                 case "rock":
-//                     if(computerChoice === "paper") {
-//                         setGameResult('You lost');
-//                         setComputerScore((prevScore) => prevScore + 1);
-//                     } else if (computerChoice === "scissors") {
-//                         setGameResult('You won');
-//                         setUserScore((prevScore) => prevScore + 1);
-//                     } else {
-//                         setGameResult('Draw');
-//                     }
-//                  break;
-//                 case "paper":
-//                     if(computerChoice === "scissors") {
-//                         setGameResult('You lost');
-//                         setComputerScore((prevScore) => prevScore + 1);
-//                     } else if (computerChoice === "rock") {
-//                         setGameResult('You won');
-//                         setUserScore((prevScore) => prevScore + 1);
-//                     } else {
-//                         setGameResult('Draw');
-//                     }
-//                  break;
-//                 case "scissors":
-//                     if(computerChoice === "rock") {
-//                         setGameResult('You lost');
-//                         setComputerScore((prevScore) => prevScore + 1);
-//                     } else if (computerChoice === "paper") {
-//                         setGameResult('You won');
-//                         setUserScore((prevScore) => prevScore + 1);
-//                     } else {
-//                         setGameResult('Draw');
-//                     }
-//                 break;
-//                 default: break;
-//             }
-//     }
 
 const compareChoices = () => {
     if (!userChoice || !computerChoice) return null;
@@ -75,7 +34,7 @@ const compareChoices = () => {
             } else {
                 setGameResult('Draw');
             }
-            break; // Ensure this break is here
+            break;
         case "paper":
             if(computerChoice === "scissors") {
                 setGameResult('You lost');
@@ -86,7 +45,7 @@ const compareChoices = () => {
             } else {
                 setGameResult('Draw');
             }
-            break; // Ensure this break is here
+            break;
         case "scissors":
             if(computerChoice === "rock") {
                 setGameResult('You lost');
@@ -97,20 +56,24 @@ const compareChoices = () => {
             } else {
                 setGameResult('Draw');
             }
-            break; // Ensure this break is here
-        default:
-            // Handle the default case appropriately if needed
             break;
+        default: break;
     }
 }
 
+    const resetGame = () => {
+        setIsUserPlaying(false);
+        setComputerChoice(null);
+        setUserChoice(null);
+        setGameResult(null);
+    }
 
      const playUser = (choice: playOptions) => {
             setIsUserPlaying(true);
             setUserChoice(choice);
             const playComputerHandler = setTimeout(() => {
                 playComputer();
-            }, 0);
+            }, 100);
 
             return () => {
                 clearTimeout(playComputerHandler);
@@ -118,10 +81,16 @@ const compareChoices = () => {
      };
 
      useEffect(() => {
-        compareChoices();
-        setIsUserPlaying(false);
-//         setComputerChoice(null);
-//         setUserChoice(null);
+         if (computerChoice && userChoice) {
+            compareChoices();
+         };
+         const resetGameHandler = setTimeout(() => {
+              resetGame();
+         }, 3000);
+
+         return () => {
+               clearTimeout(resetGameHandler);
+         }
      }, [userChoice, computerChoice]);
 
 
@@ -129,14 +98,15 @@ const compareChoices = () => {
    return (
    <>
          <button disabled={isUserPlaying} type="button" name={"rock"} onClick={() => playUser("rock")}>Rock</button>
-         <button disabled={isUserPlaying} type="button" name={"paper"} onClick={(e) => playUser("paper")}>Paper</button>
-         <button disabled={isUserPlaying} type="button" name={"scissors"} onClick={(e) => playUser("scissors")}>Scissors</button>
+         <button disabled={isUserPlaying} type="button" name={"paper"} onClick={() => playUser("paper")}>Paper</button>
+         <button disabled={isUserPlaying} type="button" name={"scissors"} onClick={() => playUser("scissors")}>Scissors</button>
 
          <div>User choice: {userChoice}</div>
          <div>Computer choice: {computerChoice}</div>
          <div>Game result: {gameResult}</div>
-         <div> Persistent user score: {userScore}</div>
-         <div> Persistent computer score: {computerScore}</div>
-         </>
+         <div>User score: {userScore}</div>
+         <div>Computer score: {computerScore}</div>
+
+   </>
   );
 };
