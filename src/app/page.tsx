dc2673cad576621d8420c67bc1ad76e3ffc33a6b
agacia1805 <agata.dopierala@vaimo.com>
2023-12-01 22:08:29 +0000

    (styling) update page styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import {Game} from './components/game';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
+    <main className="flex min-h-screen flex-col items-center p-24 bg-blue-600 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 ">
         <Game />
     </main>
   )
