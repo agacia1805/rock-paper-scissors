8ea7ce99f79a5d8f8ad2157dc0983274dff2e02c
agacia1805 <agata.dopierala@vaimo.com>
2023-12-04 19:43:02 +0000

    (feat) create footer component

---
+++ src/app/components/playOption.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+
+interface Props {
+  disabled: boolean;
+  onClick: () => void;
+  name: string;
+  label: string;
+  className?: string;
+}
+
+export const Button = ({disabled, onClick, name, label, className}: Props) => {
+   return (
+         <button
+            className={`p-8 cursor-pointer rounded-full border-8 relative border-dotted hover:scale-110 border-pink-200 ${className || ""}`}
+            disabled={disabled}
+            type="button"
+            name={name}
+            onClick={onClick}
+         >
+            <Image src={`/${name}.svg`} alt={`Choose your fighter: ${name}`} width="80" height="80" className="w-12 h-12 md:w-20 md:h-20"/>
+         </button>
+  );
+};
