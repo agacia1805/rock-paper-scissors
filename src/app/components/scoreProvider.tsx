6c41f8b5bbb10d9ecf54718a43092eeae70c3b13
agacia1805 <agata.dopierala@vaimo.com>
2023-12-08 12:36:23 +0000

    (typing) improve typing for provider

--- src/app/components/scoreProvider.tsx
+++ src/app/components/scoreProvider.tsx
@@ -1,21 +1,36 @@
 "use client";
 
-import React, { createContext, useContext } from 'react';
+import React, { createContext, useContext, ReactNode } from 'react';
 import useLocalStorage from '../hooks/useLocalStorage';
 
-const ScoreContext = createContext({
-  score: 0,
-  setScore: (score: number) => {},
-});
+type ScoreContextType = {
+  score: number;
+  setScore: (newValue: number | ((val: number) => number)) => void;
+};
 
-export const useScore = () => useContext(ScoreContext);
+const ScoreContext = createContext<ScoreContextType>({
+  score: 0,
+  setScore: () => {},
+});
 
 export const ScoreProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
   const [score, setScore] = useLocalStorage<number>('score', 0);
 
+  const updateScore = (newValue: number | ((val: number) => number)) => {
+      setScore(prevScore => (typeof newValue === 'function' ? newValue(prevScore) : newValue));
+  };
+
   return (
-    <ScoreContext.Provider value={{ score, setScore }}>
+    <ScoreContext.Provider value={{ score, setScore: updateScore }}>
       {children}
     </ScoreContext.Provider>
   );
+};
+
+export const useScore = () => {
+    const context = useContext(ScoreContext);
+    if (!context) {
+        throw new Error('useScore must be used within a ScoreProvider');
+    }
+    return context;
 };
