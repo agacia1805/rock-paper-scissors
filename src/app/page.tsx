10d8ce12a01b15b52dd80dc17c4de2e50005ebbf
agacia1805 <agata.dopierala@vaimo.com>
2023-12-08 13:35:28 +0000

    (chore) update page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,9 @@ import {Header} from './components/header';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center px-4 py-12 bg-blue-600 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 gap-32">
+    <main className="flex min-h-screen flex-col items-center px-4 pt-12 pb-4 bg-blue-600 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 gap-32">
         <Header/>
         <Game />
-
     </main>
   )
 }
