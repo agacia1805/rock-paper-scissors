883e3e8d68178d24215fbafbab5e8371cc558f62
agacia1805 <agata.dopierala@vaimo.com>
2023-12-08 12:55:29 +0000

    (style) update loader styles to match play option

--- src/app/components/loader.tsx
+++ src/app/components/loader.tsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 export const Loader = () => {
    return (
          <motion.div
-             className="w-40 h-40 border-8 border-dotted rounded-full border-pink-200"
+             className="w-32 h-32 md:w-40 md:h-40 border-8 border-dotted rounded-full border-pink-200"
              animate={{ rotate: 360 }}
              transition={{ duration: 6, repeat: Infinity, ease: "linear" }}
          >
