f894fa378bf9e08c25a8256ed5fb799d4a4ad400
agacia1805 <agacia1805@gmail.com>
2023-12-13 18:15:48 +0100

    (footer) include footer on the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
-import Image from 'next/image';
 import { Game } from './components/game';
-// import {Footer} from './components/footer';
+import {Footer} from './components/footer';
 import { Header } from './components/header';
 
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center gap-32 bg-blue-600 bg-opacity-10 bg-clip-padding px-4 pb-4 pt-12 backdrop-blur-md backdrop-filter'>
       <Header />
-      <Game />
+      <div className="flex-grow">
+        <Game />
+      </div>
+      <Footer />
     </main>
   );
 }
